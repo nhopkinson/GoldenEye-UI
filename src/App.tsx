908703bac75a83df16e8fadbcc999a0cf4c2fb53
Nigel Hopkinson <hopkinson.nigel@gmail.com>
2018-11-14 15:46:58 -0500

    first commit of new branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,71 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as React from "react";
+import "./App.css";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <nav className="topNav">
+          <a href="/">Home </a>
+          <a href="/about">About </a>
+          <a href="/contact">Contact </a>
+          <br />
+        </nav>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to goldeneye.ui</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+        <p className="App-intro"> Use this app to store files of all types.</p>
+
+        <form className="Form" action="/signin" method="post">
+          <p>
+            <label>
+              <b />
+              Sign In <br /> <br />
+              First name
+            </label>
+            <br />
+            <input type="text" name="first_name" />
+          </p>
+          <p>
+            <label>Last name</label>
+            <br />
+            <input type="text" name="last_name" />
+          </p>
+          <p>
+            <label>Email</label>
+            <br />
+            <input type="email" name="email" />
+          </p>
+          <p>
+            <label>Cell Phone number</label>
+            <br />
+            <input type="tel" name="phone" />
+          </p>
+          <p>
+            <label>Password</label>
+            <br />
+            <input type="password" name="password" />
+          </p>
+
+          <p>
+            <label>
+              <input type="checkbox" value="terms" />I agree to the{" "}
+              <a href="/terms">terms and conditions</a>
+            </label>
+          </p>
+          <p>
+            <button>Sign up</button>
+            <button type="reset">Reset form</button>
+          </p>
+        </form>
+
+        <p>
+          <form className="Upload">
+            <fieldset>
+              <legend>Upload your file: </legend>
+              <button>Upload</button>
+            </fieldset>
+          </form>
         </p>
       </div>
     );
