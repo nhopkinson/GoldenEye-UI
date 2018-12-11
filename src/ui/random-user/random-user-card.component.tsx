ef830a370836eba682e346e7c9fd429d4627b527
Nigel Hopkinson <hopkinson.nigel@gmail.com>
2018-12-11 17:51:12 -0500

    wip

--- src/ui/random-user/random-user-card.component.tsx
+++ src/ui/random-user/random-user-card.component.tsx
@@ -1,64 +1,28 @@
 import * as React from 'react';
 import { Grid, Panel, Row } from 'react-bootstrap';
-import { Button } from './button';
 
-interface RandomUserCardState {
-  user: User;
+interface RandomUserCardProps {
+  picture: string;
+  name: string;
+  email: string;
 }
 
-class User {
-  public name: string = '';
-  public email: string = '';
-  public picture: string = '';
-}
-
-// tslint:disable-next-line:max-classes-per-file
-export class RandomUserCard extends React.Component<{}, RandomUserCardState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      user: new User(),
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  public handleClick(): void {
-    this.getRandomUser();
-  }
-
-  public async componentDidMount(): Promise<void> {
-    this.getRandomUser();
-  }
-
-  public async getRandomUser(): Promise<void> {
-    const apiUrl = 'https://randomuser.me/api/';
-
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
-    const user = data.results[0];
-    const { name, picture } = user;
-
-    this.setState({ user: { name: name.first, email: user.email, picture: picture.medium } });
-  }
-
+export class RandomUserCard extends React.Component<RandomUserCardProps> {
   public render(): JSX.Element {
+    const { picture, name, email } = this.props;
+
     return (
       <Panel>
         <Panel.Body>
           <Grid>
             <Row>
-              <img src={this.state.user.picture} />
+              <img src={picture} />
             </Row>
             <Row>
-              <strong>Name: </strong> {this.state.user.name}
+              <strong>Name: </strong> {name}
             </Row>
             <Row>
-              <strong>Email: </strong> {this.state.user.email}
-            </Row>
-            <Row>
-              <Button btnLabel="Generate User" handleClick={this.handleClick} />
+              <strong>Email: </strong> {email}
             </Row>
           </Grid>
         </Panel.Body>
--- src/ui/random-user/random-user.component.tsx
+++ src/ui/random-user/random-user.component.tsx
@@ -1,8 +1,44 @@
 import * as React from 'react';
+import { Button } from './button';
 import { RandomUserCard } from './random-user-card.component';
+import { User } from './User';
+
+interface RandomUserState {
+  user: User;
+}
 
 export class RandomUser extends React.Component {
+  public state: RandomUserState = {
+    user: new User(),
+  };
+
+  public handleClick = (): void => {
+    this.getRandomUser();
+  };
+
+  public componentDidMount(): void {
+    this.getRandomUser();
+  }
+
+  public async getRandomUser(): Promise<void> {
+    const apiUrl = 'https://randomuser.me/api/';
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const user = data.results[0];
+    const { name, picture } = user;
+
+    this.setState({ user: { name: name.first, email: user.email, picture: picture.medium } });
+  }
   public render(): JSX.Element {
-    return <RandomUserCard />;
+    const { picture, name, email } = this.state.user;
+
+    return (
+      <>
+        <RandomUserCard name={name} picture={picture} email={email} />
+        <Button btnLabel="Generate User" handleClick={this.handleClick} />
+      </>
+    );
   }
 }
